0a6a0f29ae4bdd0dd55e8e8c5ed7dac72d649b75
longtermtobi <tbfullstack@gmail.com>
2019-12-30 14:18:40 +0000

    Assign objects to variables with names easy to remmember, for better writing and readability

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-alert('Yoo');
+const fullDate = new Date(); // newDate is an object that represents a date and time.
+const mins = fullDate.getMinutes(); // getMinutes, gets the minutes out of new Date.
+const hours = fullDate.getHours(); // getHours, gets the hours out of new Date.
+const secs = fullDate.getSeconds(); //getSeconds, gets the seconds out of new Date
