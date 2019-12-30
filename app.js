7a74bcd671da95df59fe8ecaaa7224aa2c2afb41
longtermtobi <tbfullstack@gmail.com>
2019-12-30 14:41:30 +0000

    finished the app.js finished clock and added setInterval comment

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const fullDate = new Date(); // newDate is an object that represents a date and time.
-const mins = fullDate.getMinutes(); // getMinutes, gets the minutes out of new Date.
-const hours = fullDate.getHours(); // getHours, gets the hours out of new Date.
-const secs = fullDate.getSeconds(); //getSeconds, gets the seconds out of new Date
+function tester() {
+  const fullDate = new Date(); // newDate is an object that represents a date and time.
+  const mins = fullDate.getMinutes(); // getMinutes, gets the minutes out of new Date.
+  const hours = fullDate.getHours(); // getHours, gets the hours out of new Date.
+  const secs = fullDate.getSeconds(); //getSeconds, gets the seconds out of new Date
+
+  document.querySelector('#hour').innerHTML = hours;
+  document.querySelector('#minute').innerHTML = mins;
+  document.querySelector('#second').innerHTML = secs;
+}
+
+setInterval(tester, 1000); // Makes the function update every second
