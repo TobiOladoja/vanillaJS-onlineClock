a465b93c1f1014ae0be3c67d94743ffc602fc4a6
longtermtobi <tbfullstack@gmail.com>
2019-12-30 14:46:10 +0000

    finishing things up, changed const to let so I could update seconds to add 0 when its counting below 10

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 function tester() {
-  const fullDate = new Date(); // newDate is an object that represents a date and time.
-  const mins = fullDate.getMinutes(); // getMinutes, gets the minutes out of new Date.
-  const hours = fullDate.getHours(); // getHours, gets the hours out of new Date.
-  const secs = fullDate.getSeconds(); //getSeconds, gets the seconds out of new Date
+  let fullDate = new Date(); // newDate is an object that represents a date and time.
+  let mins = fullDate.getMinutes(); // getMinutes, gets the minutes out of new Date.
+  let hours = fullDate.getHours(); // getHours, gets the hours out of new Date.
+  let secs = fullDate.getSeconds(); //getSeconds, gets the seconds out of new Date
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  if (secs < 10) {
+    secs = `0${secs}`;
+  }
 
   document.querySelector('#hour').innerHTML = hours;
-  document.querySelector('#minute').innerHTML = mins;
-  document.querySelector('#second').innerHTML = secs;
+  document.querySelector('#minute').innerHTML = `:${mins}`;
+  document.querySelector('#second').innerHTML = `:${secs}`;
 }
 
 setInterval(tester, 1000); // Makes the function update every second
